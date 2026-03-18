215a2a2609283fa9a932328804dd391a90530f8a
Pablo Zabala <could_2032@outlook.com>
2026-03-18 04:27:49 -0300

    instalando react + vite + tailwind

---
+++ Portafolio/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ return (
+  <>
+    <div className="App bg-green-800 min-h-screen flex flex-row   items-center  justify-center">
+      <h1 className='text-xl bg-blue-500 text-white p-2 rounded'>Vite React App</h1>
+    </div>
+
+  </>
+ )
+}
+
+export default App
