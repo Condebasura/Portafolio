3f0941355100d681e85f7f052ce724c53f660eaa
Pablo Zabala <could_2032@outlook.com>
2026-03-21 20:36:15 -0300

    Continuando con las tecnologias usadas

--- Portafolio/src/App.tsx
+++ Portafolio/src/App.tsx
@@ -18,10 +18,29 @@ import ImgProfile from "./assets/foto_perfil.jpg";
       </Nav>
     </div>
     
-      <Seccion title="Pablo César Zabala"parraph="Full-Stack Web Developer" className="text-start p-2 m-4 bg-gray-100 rounded-lg flex flex-row gap-4" title_2="I like to create complete projects, from the backend to the frontend" imagen={ImgProfile}/>
+      <Seccion title="Pablo César Zabala"parraph="Full-Stack Web Developer" className="text-start p-2 m-4 bg-gray-100 rounded-lg flex flex-row gap-4 " title_2="I like to create complete projects, from the backend to the frontend" imagen={ImgProfile}/>
     
-    <div className="About-Me">
-
+    <div className="About-Me bg-gray-100 rounded-lg  aspect-3/2 object-cover">
+      <Seccion title="About Me" parraph="I'm a developer with experience in HTML, CSS, Bootstrap, Tailwind, JavaScript, Node.js, Express, SQLite, and Git. I'm currently expanding my knowledge of React, Electron, TypeScript, cloud services, working with instances from CloudShell and PuTTY, as well as tools like Apache2 and Linux environments . I am a passionate  with a strong background in both frontend and backend development. I enjoy creating complete projects that encompass all aspects of web development, from designing user interfaces to implementing server-side logic. With a keen eye for detail and a commitment to delivering high-quality work, I strive to build seamless and efficient web applications that provide an exceptional user experience." 
+      title_2="technologies I use" Fonts={[
+       { title: "HTML", className: "devicon-html5-plain colored text-4xl" },
+       { title: "CSS", className: "devicon-css3-plain colored text-4xl" },
+       { title: "JavaScript", className: "devicon-javascript-plain colored text-4xl" },
+       {title:"tipescript", className: "devicon-typescript-plain colored text-4xl"},
+       {title:"bootstrap", className: "devicon-bootstrap-plain colored text-4xl"},
+       {title:"tailwind", className: "devicon-tailwindcss-plain colored text-4xl"},
+       {title:"nodejs", className: "devicon-nodejs-plain colored text-4xl"},
+       {title:"express", className: "devicon-express-original colored text-4xl"},
+       {title:"sqlite", className: "devicon-sqlite-plain colored text-4xl"},
+       {title:"git", className: "devicon-git-plain colored text-4xl"},
+        {title:"react", className: "devicon-react-original colored text-4xl"},
+        {title:"electron", className: "devicon-electron-original colored text-4xl"},
+        {title:"linux", className: "devicon-linux-plain colored text-4xl"},
+        {title:"apache", className: "devicon-apache-plain colored text-4xl"},
+        {title:"cloud", className: "fa-solid fa-cloud text-4xl text-gray-950"},
+        {title:"puTTY", className: "devicon-putty-plain colored text-4xl"},
+      ]}
+      />
     </div>
         </div>
  )
--- Portafolio/src/components/seccion.tsx
+++ Portafolio/src/components/seccion.tsx
@@ -1,4 +1,10 @@
+type Font = {
+    title: string;
+    className: string;
+}
+
 type SeccionProps = {
+    Fonts?: Font[];
     title: string;
     parraph: string;
     title_2?:string;
@@ -6,7 +12,7 @@ type SeccionProps = {
     imagen?: string;
 }
 
-export default function Seccion({title , parraph , title_2 , className, imagen}: SeccionProps){
+export default function Seccion({title , parraph , title_2 , className, imagen, Fonts}: SeccionProps){
     return(
         <div className={className}>
             <div className="flex flex-col m-2">
@@ -15,6 +21,14 @@ export default function Seccion({title , parraph , title_2 , className, imagen}:
             {title_2 && <h3 className="text-xl font-semibold mb-2">{title_2}</h3>}
             </div>
             {imagen && <img src={imagen} alt="Profile" className="rounded-lg size-56" />}
+            {Fonts &&(
+                <div className="flex flex-row gap-4 m-2">
+                    {Fonts.map((font , index) => (
+                        <span key={index} className={font.className} title={font.title}>
+                        </span>
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
