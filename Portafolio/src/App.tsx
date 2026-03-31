c9efe3ac9d31c79ba672f88b19f8071e68461297
Pablo Zabala <could_2032@outlook.com>
2026-03-31 00:53:45 -0300

    Terminando con el diseño

--- Portafolio/src/App.tsx
+++ Portafolio/src/App.tsx
@@ -14,7 +14,7 @@ import Button from "./components/button";
 
     <div className=" Nav flex flex-row justify-end p-2  ">
       
-      <Nav className="text-white flex flex-row  bg-gray-200 w-max h-8 rounded-lg items-center gap-4 me-2 px-4" 
+      <Nav className="text-white flex flex-row  bg-gray-100 w-max h-8 rounded-lg items-center gap-4 me-2 px-4 shadow-xl" 
       Fonts={[{ url: "https://github.com/Condebasura", className: "fa-brands fa-github text-2xs text-gray-950", target: "_blank" },{
         url: "https://www.linkedin.com/in/pablo-césar-zabala-51055b1bb", className: "fa-brands fa-linkedin text-2xs text-gray-950", target: "_blank"
       }]}
@@ -24,12 +24,12 @@ import Button from "./components/button";
     </div>
 
      <div className="flex flex-row justify-center md:justify-end p-2 ">
-      <Seccion title="Pablo César Zabala"parraph="Full-Stack Web Developer" className=" flex md:flex-row gap-4 flex-col text-center items-center  md:text-start  w-1/2  shadow-xl p-4 m-4 bg-gray-200 rounded-lg  " title_2="I like to create complete projects, from the backend to the frontend" imagen={ImgProfile}/>
+      <Seccion title="Pablo César Zabala"parraph="Full-Stack Web Developer" className=" flex md:flex-row gap-4 flex-col text-center items-center  md:text-start  w-1/2  shadow-xl p-4 m-4 bg-gray-100 rounded-lg text-2x1  " title_2="I like to create complete projects, from the backend to the frontend" imagen={ImgProfile}/>
      </div>
     
     <div className=" flex flex-row justify-center md:justify-start">
 
-      <Seccion title="About Me" className="About-Me bg-gray-200  shadow-xl wrap-anywhere rounded-lg flex flex-col w-1/2  gap-4 m-4 p-4 " parraph="I'm a developer with experience in HTML, CSS, Bootstrap, Tailwind, JavaScript, Node.js, Express, SQLite, and Git. I'm currently expanding my knowledge of React, Electron, TypeScript, cloud services, working with instances from CloudShell and PuTTY, as well as tools like Apache2 and Linux environments . I am a passionate  with a strong background in both frontend and backend development. I enjoy creating complete projects that encompass all aspects of web development, from designing user interfaces to implementing server-side logic. With a keen eye for detail and a commitment to delivering high-quality work, I strive to build seamless and efficient web applications that provide an exceptional user experience." 
+      <Seccion title="About Me" className="About-Me bg-gray-100  shadow-xl wrap-anywhere rounded-lg flex flex-col w-1/2  gap-4 m-4 p-4 " parraph="I'm a developer with experience in HTML, CSS, Bootstrap, Tailwind, JavaScript, Node.js, Express, SQLite, and Git. I'm currently expanding my knowledge of React, Electron, TypeScript, cloud services, working with instances from CloudShell and PuTTY, as well as tools like Apache2 and Linux environments . I am a passionate  with a strong background in both frontend and backend development. I enjoy creating complete projects that encompass all aspects of web development, from designing user interfaces to implementing server-side logic. With a keen eye for detail and a commitment to delivering high-quality work, I strive to build seamless and efficient web applications that provide an exceptional user experience." 
       title_2="technologies I use" Fonts={[
         { title: "HTML", className: "devicon-html5-plain colored md:text-4xl" },
         { title: "CSS", className: "devicon-css3-plain colored md:text-4xl" },
@@ -50,12 +50,17 @@ import Button from "./components/button";
       ]}
       />
       </div>
-    
-    <div className=" flex justify-center md:flex-row gap-4 md:justify-end p-2 ">
+    <div className="flex flex-row justify-center ">
 
+        <p className="text-2xl p-3 m-4 text-gray-950 underline decoration-sky-500">Projects</p>
+    </div>
+    <div className=" flex justify-center md:flex-row gap-4 md:justify-end p-2 ">
+      
+      
+        
      
     <Card tipo="Proyectos"
-    className="flex flex-col m-2  p-3 bg bg-gray-200 w-2/5  shadow-xl rounded-lg gap-4"
+    className="flex flex-col m-2  p-3 bg bg-gray-100 w-2/5  shadow-xl rounded-lg gap-4"
     Photo={ImgConsult}
     title="Management System for Medical Offices" description="A web application designed for managing appointments and clinical records in medical practices. The system allows for the administration of users, doctors, patients, appointments, and each patient's medical history within a local network."
     tecnologies={[
@@ -95,9 +100,9 @@ import Button from "./components/button";
       
     ]}
     >
-      <div className="flex flex-col md:flex-row justify-center">
+      <div className="flex flex-col justify-center md:flex-row  md:justify-start ">
 
-       { <Button  name=" GitHub" className="fa-brands fa-github flex flex-row gap-1 m-2 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-md text-xs md:text-sm  px-3 py-2 text-center ms-2 w-24" url="https://github.com/Condebasura/Loto_Indumentaria" target="_blank"/>}
+       { <Button  name=" GitHub" className="fa-brands fa-github flex flex-row gap-1 m-2 text-white bg-blue-600  hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-md text-xs md:text-sm  px-3 py-2 text-center ms-2 w-24" url="https://github.com/Condebasura/Loto_Indumentaria" target="_blank"/>}
 
        {<Button  name=" Demo" className="fa-solid fa-eye flex flex-row gap-1 m-2 text-white bg-gray-400 hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-gray-300 ms-2 rounded-md text-xs md:text-sm  px-3 py-2 text-center w-24" url="https://loto-indumentaria.hopto.org" target="_blank"/>}
       </div>
@@ -124,7 +129,7 @@ import Button from "./components/button";
       
     ]}
     >
-      <div className="flex flex-col md:flex-row md:justify-center">
+      <div className="flex flex-col md:flex-row justify-center md:justify-start">
 
        { <Button  name=" GitHub" className="fa-brands fa-github flex flex-row gap-1 m-2 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-md text-xs px-3 py-2 text-center ms-2 w-24" url="https://github.com/Condebasura/Login-Create" target="_blank"/>}
 
--- Portafolio/src/components/seccion.tsx
+++ Portafolio/src/components/seccion.tsx
@@ -17,8 +17,8 @@ export default function Seccion({title , parraph , title_2 , className, imagen, 
         <div className={className}>
             {imagen && <img src={imagen} alt="Profile" className="rounded-lg size-56" />}
             <div className="flex flex-col m-2">
-            <h1 className="text-2xl font-bold mb-4">{title}</h1>
-            <p className="text-gray-700 mb-4 ">{parraph}</p>
+            <h1 className="text-2xl font-bold mb-1">{title}</h1>
+            <p className="text-gray-700 mb-4  ms-2">{parraph}</p>
             {title_2 && <h3 className="text-xl font-semibold mb-2">{title_2}</h3>}
             </div>
             {Fonts &&(
